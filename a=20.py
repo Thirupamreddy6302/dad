2b4b603449e30f01082303f3989eeda658b38ed2
Thirupamreddy6302 <43023509+Thirupamreddy6302@users.noreply.github.com>
2018-09-26 14:19:43 +0530

    Update a=20.py

--- a=20.py
+++ a=20.py
@@ -7,3 +7,12 @@ elif(a==b)
 	print(a,"is equal to",b)
 elif(b>a)
     print(b,"is greater than",a)
+	
+OUTPUT:
+	python3 a=20.py
+enter a value of a: 20
+enter a value of b: 40
+40 is greater than 20
+
+	
+	
