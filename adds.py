8c877a9fcaf50d55aa9b6738d17b0979ca93e18d
Thirupamreddy6302 <43023509+Thirupamreddy6302@users.noreply.github.com>
2018-09-26 14:21:23 +0530

    Update adds.py

--- adds.py
+++ adds.py
@@ -7,3 +7,9 @@ elif(a==b)
 	print(a,"is equal to",b)
 elif(b>a)
     print(b,"is greater than",a)
+	
+OUTPUT:	
+	 python3 adds.py
+enter a value of a: 54
+enter a value of b: 34
+54 is greater than 34
