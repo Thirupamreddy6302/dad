eccfe121aa5281b614adf4a7d62e9eba166579f1
Thirupamreddy6302 <43023509+Thirupamreddy6302@users.noreply.github.com>
2018-10-25 09:56:59 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,3 @@
+import os
+os.system("fswebcam -F 4 --fps 20 -r 800*600 /home/user/Desktop/1.jpg")
+print("pic taken")
---
+++ gpio.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+pirPin= 8
+GPIO.setup(pirPin, GPIO.IN)
+counter=1
+time.sleep(4)
+
+while True:
+    if GPIO.input(pirPin):
+        print("Motion Detected")
+        time.sleep(1)
+        counter=counter+1
+        import os
+        os.system("fswebcam -F 4 --fps 20 -r 800*600 /home/pi/Desktop/1.jpg")
+        print("pic taken")
+    else:
+        print("testing")
+        
